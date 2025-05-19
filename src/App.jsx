2596922972c71263523522e3d562ffacf651ba2b
sofiap03 <sofia.pinuelam@autonoma.edu.co>
2025-05-19 13:14:43 -0500

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
-import Registro from "./pages/Register"
+import Registro from "./pages/Register";
 import PlanSuscripcion from "./pages/PlanSuscripcion";
 import Dashboard from "./pages/Dashboard";
 import NavBar from "./components/Navbar";
 import Inventario from "./pages/Inventario";
 import Mantenimiento from "./pages/Mantenimiento";
-
+import PerfilUsuario from "./pages/PerfilUsuario";
+import ProtectedRoute from './components/ProtectedRoute'; 
+import HojadeVida from "./pages/HojaDeVida";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
+        {/* Ruta de inicio redirige a login si no hay sesión */}
         <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Rutas públicas */}
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/planSuscripcion" element={<PlanSuscripcion />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/inventario" element={<Inventario />} />
-        <Route path="/mantenimiento" element={<Mantenimiento />} />
+
+        {/* Rutas protegidas */}
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute element={<Dashboard />} />
+          }
+        />
+        <Route 
+          path="/inventario" 
+          element={
+            <ProtectedRoute element={<Inventario />} />
+          }
+        />
+        <Route 
+          path="/mantenimiento" 
+          element={
+            <ProtectedRoute element={<Mantenimiento />} />
+          }
+        />
+        <Route 
+          path="/perfil" 
+          element={
+            <ProtectedRoute element={<PerfilUsuario />} />
+          }
+        />
+        <Route 
+          path="/hojadevida" 
+          element={
+            <ProtectedRoute element={<HojadeVida />} />
+          }
+        />
       </Routes>
     </>
   );
